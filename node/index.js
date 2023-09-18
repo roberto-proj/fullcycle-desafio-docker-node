f3b95be31fdb26e185ca08f82cc317a4b53bba8d
roberto-proj <maxarthur101@hotmail.com>
2023-09-18 08:37:26 -0300

    [FEAT] - Adiciona novo requisito

--- node/index.js
+++ node/index.js
@@ -11,16 +11,14 @@ const config = {
 const mysql = require('mysql')
 const connection = mysql.createConnection(config)
 
-const sql = `INSERT INTO people(name) VALUES ('nova pessoa')`
-connection.query(sql)
 
 app.get('/', (req,res) => {
+    connection.query(`INSERT INTO people(name) VALUES ('nova pessoa')`)
     connection.query(`SELECT * FROM people`, function (err, result, fields) {
         res.send('<h1>Full Cycle Rocks!</h1>'+'<ul>'+result.map(a => {
             return `<li>id: ${a.id} nome: ${a.name}</li>`;
         })+'</ul>')
     })
-    connection.end()
 })
 
 app.listen(port, ()=> {
